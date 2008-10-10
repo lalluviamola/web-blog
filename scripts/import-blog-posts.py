887d6a3542e296aaa089ed3e947a71da0456a7e2
kkowalczyk <kkowalczyk@experien-186ae0.(none)>
2008-10-10 00:22:23 -0700

    start on posts import

--- scripts/import-blog-posts.py
+++ scripts/import-blog-posts.py
@@ -2,13 +2,23 @@
 
 import sys, os.path, sqlite3
 import postsparse
+import genblog
 
-# Directory
-g_db_dir = None
-g_db_path = None
+_g_script_dir = None
+def script_dir():
+    global _g_script_dir
+    if _g_script_dir is None:
+        _g_script_dir = os.path.dirname(os.path.realpath(__file__))
+    return _g_script_dir
+
+def db_dir():
+    return script_dir()
 
 def db_path():
-    return os.path.join(g_db_dir, "blog.db")
+    return os.path.join(db_dir(), "blog.db")
+
+def posts_dir():
+    return os.path.join(script_dir(), "..", "srcblog")
 
 CREATE_DB_1_SQL = """
 -- immutable piece of content (text). The actual content is stored in a file
@@ -40,25 +50,29 @@ CREATE TABLE posts (
 # This is for one-time only import, so assume that if the database file exists
 # we've already imported the posts
 def init_db():
-    path = db_path()
-    if os.path.exists(path):
-        print("Database '%s' already exists!" % path)
+    dbpath = db_path()
+    if os.path.exists(dbpath):
+        print("Database '%s' already exists!" % dbpath)
         sys.exit(1)
-    conn = sqlite3.connect(path)
+    conn = sqlite3.connect(dbpath)
     c = conn.cursor()
     c.execute(CREATE_DB_1_SQL)
     c.execute(CREATE_DB_2_SQL)
     conn.commit()
     c.close()
 
-def calc_dirs():
-    global g_db_dir
-    g_db_dir = os.path.dirname(os.path.realpath(__file__))
-    print("g_db_dir = '%s'" % g_db_dir)
+def import_posts():
+    post_files = postsparse.scan_posts(posts_dir())
+    posts = post_files.values()
+    posts.sort(lambda x,y: cmp(x["date"], y["date"]))    
+    genblog.gen_urls(posts)
+    for p in posts:
+        url = p["url"];
+        print("url: %s" % url)
 
 def main():
-    calc_dirs()
-    init_db()
+    #init_db()
+    import_posts()
 
 if __name__ == "__main__":
     main()
