f526c88e671abcc55b635e8a02ff57223fb884b2
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-02-17 02:52:20 -0800

    one more feed redirect

--- main.py
+++ main.py
@@ -916,6 +916,7 @@ def main():
         ('/app/clearmemcache', ClearMemcacheHandler),
         ('/feed/rss2/atom.xml', OldAtomRedirectHandler),
         ('/feed/rss2/', OldAtomRedirectHandler),
+        ('/feed/rss2', OldAtomRedirectHandler),
         # only enable /import before importing and disable right
         # after importing, since it's not protected
         #('/import', ImportHandler),
