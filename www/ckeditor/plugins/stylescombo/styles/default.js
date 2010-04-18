196ea8641c291f4f90ea6a19d447f224370a0b6f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2010-04-17 23:59:37 -0700

    remove ckeditor

--- www/ckeditor/plugins/stylescombo/styles/default.js
+++
@@ -1,9 +0,0 @@
-﻿/*
-Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
-
-CKEDITOR.stylesSet.add('default',[
-{name:'Inline Quotation',element:'q'},
-{name:'Computer Code',element:'code'},
-]);
